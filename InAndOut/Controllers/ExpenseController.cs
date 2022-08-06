d1f7d1f212d7843bcdcc710068dcd71ace13975c
emin <akyuzz.eminn@gmail.com>
2022-08-06 16:30:21 +0300

    Add project files.

---
+++ InAndOut/Controllers/ExpenseController.cs
@@ -0,0 +1,128 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ExpenseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objList = _db.Expenses;
+            return View(objList);
+
+        }
+
+
+        //GET-Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        //POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense obj)
+        {
+            if (ModelState.IsValid) //server side boş deger kontrolü
+            {
+                _db.Expenses.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+            
+        }
+
+
+
+        //Get Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id==null || id==0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+
+        public async Task<IActionResult> DeleteExpense(int? id)
+        {
+            Expense exp = await _db.Expenses.FindAsync(id);
+            _db.Remove(exp);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        //Post Delete
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult DeletePost(int? id)
+        //{
+        //    var obj = _db.Expenses.Find(id);
+        //    if (obj == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _db.Expenses.Remove(obj);
+        //    _db.SaveChanges();
+        //    return RedirectToAction("Index");
+
+        //}
+
+
+
+        //Get Update
+        public IActionResult Update(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Expenses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        //POST-Update
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Expense obj)
+        {
+            if (ModelState.IsValid) //server side boş deger kontrolü
+            {
+                _db.Expenses.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+    }
+}
